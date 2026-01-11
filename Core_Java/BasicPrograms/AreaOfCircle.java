cff4e08c064b3ef82d45d16fcd841274055e7cd5
Sriram Akula <sriramakula212@gmail.com>
2026-01-11 21:23:54 +0530

    Cleaned build files from repo

--- Core_Java/BasicPrograms/AreaOfCircle.java
+++
@@ -1,8 +0,0 @@
-
-public class AreaOfCircle {
-    public static void main(String[] args){
-        int radius = 5;
-        double area = Math.PI*radius*radius;
-        System.out.printf("Area of Circle: %.2f",area);
-    }
-}
