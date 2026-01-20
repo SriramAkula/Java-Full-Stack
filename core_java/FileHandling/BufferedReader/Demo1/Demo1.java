96127fdf7571a3855708e94e18b9cdf607900af9
Sriram Akula <sriramakula212@gmail.com>
2026-01-20 16:04:37 +0530

    Worked on File Handling

---
+++ core_java/FileHandling/BufferedReader/Demo1/Demo1.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        String path="C:\\Users\\srira\\Desktop\\Java-Full-Stack\\Core_Java\\FileHandling\\BufferedWriter\\Demo1\\employees.txt";
+        String line;
+        try(BufferedReader br=new BufferedReader(new FileReader(path))){
+            line=br.readLine();
+
+            while(line!=null){
+                System.out.println(line);
+                line=br.readLine();
+            }
+            br.close();
+        }catch(Exception e){
+            e.getMessage();
+            e.printStackTrace();
+        }
+    }
+}
