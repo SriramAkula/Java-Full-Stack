b620413b477f44d80bd85dd0e7fb070b6f82fc2c
Sriram Akula <sriramakula212@gmail.com>
2026-02-27 15:09:17 +0530

    SPring-MVC

---
+++ All-Capg-Workspaces/Capg-Adv-Java-Workspace/Maven4_Junit4/src/main/java/com/capg/Junit4/App.java
@@ -0,0 +1,13 @@
+package com.capg.Junit4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
